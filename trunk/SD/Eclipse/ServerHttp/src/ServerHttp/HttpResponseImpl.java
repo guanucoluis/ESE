71c88f5d3814b4a3dc978d5e2c14ca787bc2f2c8
fbocalon <francobocalon@gmail.com>
2016-04-21 21:10:04 -0300

    agregado implementacion Santiago

--- trunk/SD/Eclipse/ServerHttp/src/ServerHttp/HttpResponseImpl.java
+++ trunk/SD/Eclipse/ServerHttp/src/ServerHttp/HttpResponseImpl.java
@@ -2,49 +2,41 @@ package ServerHttp;
 
 public class HttpResponseImpl implements HttpResponse{
 	
-public HttpResponseImpl(){
-}
-
+	// write the http response
 	@Override
-	public String getStatus404() {
-		// TODO Auto-generated method stub
+	public String getStatus404(){
+	return ("HTTP/1.1 404 Not Found");
+	}
+	@Override
+	public String getStatus405(){
+	return ("HTTP/1.1 405 Method Not Allowed");
+	}
+	@Override
+	public String getStatus500(){
+	return("HTTP/1.1 500 Internal Server Error");
+	}
+	@Override
+	public String getStatus503(){
+	return("HTTP/1.1 503 Service Unavailable");
+	}
+	@Override
+	public void addHeader(String name, String value){
+	 //completar
+	}
+	@Override
+	public String getHeaders(){
+	//completar
 		return null;
 	}
-
 	@Override
-	public String getStatus405() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getResponseHeaderOK(){
+	return("HTTP/1.1 200 OK\r\n<html>Server: Mi propio Server\r\nContent-Type:text/html\r\n</html>\r\n");
+	//out.println("");
+	//out.println("<html>");
+	//out.println("<head><title>WEb simple grupo 5</title></head>");
+	//out.println("<body>");
+	//out.println("<h1>Todo bien por aca</h1>");
+	//out.println("<//body>");
+	//out.println("</html>");
 	}
-
-	@Override
-	public String getStatus500() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getStatus503() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addHeader(String name, String value) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public String getHeaders() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getResponseHeaderOK() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
